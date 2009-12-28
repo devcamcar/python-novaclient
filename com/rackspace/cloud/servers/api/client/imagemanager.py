9cdd8366997dd44303a8784584923967f837a3ef
Michael Mayo <greenisus@gmail.com>
2009-12-27 17:47:07 -0800

    find and wait

--- com/rackspace/cloud/servers/api/client/imagemanager.py
+++ com/rackspace/cloud/servers/api/client/imagemanager.py
@@ -6,10 +6,13 @@
 ImageManager - EntityManager for managing image entities.
 """
 
+from datetime import datetime
+import time
+
 from com.rackspace.cloud.servers.api.client.entitymanager import EntityManager
 from com.rackspace.cloud.servers.api.client.entitylist import EntityList
 
-from com.rackspace.cloud.servers.api.client.errors import BadMethodFault
+from com.rackspace.cloud.servers.api.client.errors import BadMethodFault, NotImplementedException, CloudServersAPIFault
 from com.rackspace.cloud.servers.api.client.image import Image
 
 """
@@ -40,8 +43,41 @@ class ImageManager(EntityManager):
         raise _bmf
 
     def refresh(self, entity):
-        "Not implemented by this class, by design."
-        raise _bmf
+        entity.initFromResultDict(self.imageDetails(entity.id))
+        entity._manager = self
+
+    def find(self, id):
+        """
+        Find the image given by `id` and returns an Image object filled with
+        data from the API or None if the `id` can't be found.
+        """
+        try:
+            detailsDict = self.imageDetails(id)
+        except CloudServersAPIFault, e:
+            if e.code == 404:   # not found
+                return None     # just return None
+            else:               # some other exception, just re-raise
+                raise
+
+        retImage = Image("")
+        retImage.initFromResultDict(detailsDict)
+        retImage._manager = self
+        return retImage
+
+    def imageDetails(self, id):
+        """
+        Gets details dictionary for image with `id`.  If the image can't
+        be found, returns None
+        """
+        retDict = None
+        ret = self._GET(id, { "now": str(datetime.now()) })
+        try:
+            retDict = ret["image"]
+        except KeyError, e:
+            retDict = None
+
+        return retDict
+
 
     #
     # Polling Operations
