2152ee5e5f67d8a8a4a660e7b80cd3bedea3ccbe
Mike Mayo <greenisus@gmail.com>
2010-07-20 15:45:38 -0700

    remove account info

--- com/rackspace/cloud/servers/api/client/tests/account.py
+++ com/rackspace/cloud/servers/api/client/tests/account.py
@@ -1,7 +1,5 @@
-# RS_UN = "your user name"
-# RS_KEY = "your api key"
-RS_UN = "greenisus"
-RS_KEY = "1c331a7a4a6eb58ca6072afe81e812d0"
+RS_UN = "your user name"
+RS_KEY = "your api key"
 
 
 __all__ =(RS_UN, RS_KEY)
